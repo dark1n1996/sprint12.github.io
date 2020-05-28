79e0aded4d565099fbc5b9d1651bcdeae7b5a5a7
Vladislav Geyer <vladislav.geyer@yandex.ru>
2020-05-28 18:32:54 +0300

    Modified with module path

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const { PORT = 3000 } = process.env;
 const users = require('./routes/users');
 const cards = require('./routes/cards');
 const error = require('./routes/error');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(users);
 app.use(cards);
 app.use(error);
