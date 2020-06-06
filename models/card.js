6b79f8329529afaecc72a54a0b1d14e59fe7afe7
Vladislav Geyer <vladislav.geyer@yandex.ru>
2020-06-06 15:04:04 +0300

    Создание схем и моделей для пользователей и карточек

---
+++ models/card.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 20,
+    minlength: 2,
+  },
+  link: {
+    type: String,
+    required: true,
+    match: '',
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    default: [],
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model('card', cardSchema);
