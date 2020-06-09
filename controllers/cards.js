11f2acc1aecd228c8c6f37ad6fd40a83514b38f4
Vladislav Geyer <vladislav.geyer@yandex.ru>
2020-06-09 18:02:33 +0300

    Скорректированы контроллеры для правильного отображения ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,19 +2,25 @@ const Card = require('../models/card');
 
 const readCards = (req, res) => {
   Card.find({})
-    .then((card) => res.send({ data: card }))
+    .then((card) => res.status(200).send({ data: card }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 const createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.send({ data: card }))
+    .then((card) => res.status(200).send({ data: card }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 const deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.id)
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(404).send({ message: 'Такой карточки нет' }));
+    .then((card) => {
+      if (card) {
+        res.status(200).send({ data: card });
+      } else {
+        res.status(404).send({ message: 'Такой карточки нет' });
+      }
+    })
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 module.exports = {
   deleteCard,
